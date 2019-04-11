8a0062cea2928c90013cb09211063aef6df863a4
Miki Takahashi <math.micky.dtl@gmail.com>
2019-04-12 03:53:04 +0900

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by Miki Takahashi on 2019/04/12.
+//  Copyright © 2019 MikiTakahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
